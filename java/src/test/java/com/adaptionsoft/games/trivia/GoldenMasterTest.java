10f45329e98a4de8010fc1a569b09337f3f91676
Filippo Liverani <fili_live@yahoo.it>
2013-07-02 23:44:19 +0200

    created GoldenMasterTest

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
@@ -0,0 +1,12 @@
+package com.adaptionsoft.games.trivia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GoldenMasterTest {
+
+    @Test
+    public void goldenMasterMatchesMultipleTriviaRunsOutput() throws Exception {
+    }
+}
