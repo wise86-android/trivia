17a5b1edf0a29c05b0f839ef9b6b04d69b5a64ed
Andrea Francia <andrea@andreafrancia.it>
2013-07-03 12:21:41 +0200

    Golden Master Generator for Objective C

---
+++ objective-c/trivia/make-golden-master.sh
@@ -0,0 +1,8 @@
+xcodebuild -scheme trivia build CONFIGURATION_BUILD_DIR=build
+
+for((seed=0;seed<10;seed++)); do
+    echo Using seed:$seed
+    build/trivia $seed 2>&1 | cut -d "]" -f 2
+done > reference.txt
+
+git diff reference.txt
--- objective-c/trivia/trivia/main.m
+++ objective-c/trivia/trivia/main.m
@@ -3,6 +3,7 @@
 int main(int argc, const char * argv[])
 {
     srand ( (int)time(NULL) );
+    if(argc>1) srand(atoi(argv[1]));
     
     @autoreleasepool {
         BOOL notAWinner;
