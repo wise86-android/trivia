7af1c43c0294b37d50d6945f02ad41a6297fe9af
Filippo Liverani <fili_live@yahoo.it>
2013-07-05 13:50:56 +0200

    fixed golden maste test indentation

--- java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
@@ -15,38 +15,38 @@ import com.adaptionsoft.games.trivia.runner.GameRunner;
 
 public class GoldenMasterTest {
 
-	private static final String GOLDEN_MASTER_PATH = "golden-master.out";
-	private static final int RUNS = 100;
+    private static final String GOLDEN_MASTER_PATH = "golden-master.out";
+    private static final int RUNS = 100;
 
-	@Test
-	public void goldenMasterMatchesMultipleTriviaRunsOutput() throws Exception {
-		if (!Files.exists(Paths.get(GOLDEN_MASTER_PATH))) { 
-			writeGoldenMaster();
-		}
+    @Test
+    public void goldenMasterMatchesMultipleTriviaRunsOutput() throws Exception {
+        if (!Files.exists(Paths.get(GOLDEN_MASTER_PATH))) { 
+            writeGoldenMaster();
+        }
 
-		String goldenMasterContent = readGoldenMaster(); 
-		OutputStream outputStream = new ByteArrayOutputStream(); 
-		runTrivia(new PrintStream(outputStream)); 
+        String goldenMasterContent = readGoldenMaster(); 
+        OutputStream outputStream = new ByteArrayOutputStream(); 
+        runTrivia(new PrintStream(outputStream)); 
 
-		Assert.assertEquals(goldenMasterContent, outputStream.toString());
-	}
+        Assert.assertEquals(goldenMasterContent, outputStream.toString());
+    }
 
-	private void writeGoldenMaster() throws Exception {
-		PrintStream output = new PrintStream(GOLDEN_MASTER_PATH);
-		runTrivia(output);
-	}
+    private void writeGoldenMaster() throws Exception {
+        PrintStream output = new PrintStream(GOLDEN_MASTER_PATH);
+        runTrivia(output);
+    }
 
-	private String readGoldenMaster() throws Exception {
-		byte[] bytes = Files.readAllBytes(Paths.get(GOLDEN_MASTER_PATH));
-		return Charset.defaultCharset().decode(ByteBuffer.wrap(bytes)).toString();
-	}
+    private String readGoldenMaster() throws Exception {
+        byte[] bytes = Files.readAllBytes(Paths.get(GOLDEN_MASTER_PATH));
+        return Charset.defaultCharset().decode(ByteBuffer.wrap(bytes)).toString();
+    }
 
-	private void runTrivia(PrintStream output) {
-		System.setOut(output);
-		for (int i = 1; i <= RUNS; i++) {
-			GameRunner.main(new String[]{String.valueOf(i)});
-		}
-		output.flush();
-		System.setOut(System.out);
-	}
+    private void runTrivia(PrintStream output) {
+        System.setOut(output);
+        for (int i = 1; i <= RUNS; i++) {
+            GameRunner.main(new String[]{String.valueOf(i)});
+        }
+        output.flush();
+        System.setOut(System.out);
+    }
 }
